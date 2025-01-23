fded2b0fb8093a561ac41822374b2d7585522e8b
RishitMungra <mungrarishit@gmail.com>
2025-01-23 16:25:21 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/Login';
+import RegistrationPage from './components/Registration';
+import AccountPage from './components/AccountPage';
+import EditAccountPage from './components/EditAccount';
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/edit-account" element={<EditAccountPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/data/sampleUsers.js
@@ -0,0 +1,4 @@
+export const sampleUsers = [
+  { username: "JohnDoe", email: "john@example.com", password: "password123" },
+  { username: "abc", email: "abc@gmail.com", password: "123" },
+];
